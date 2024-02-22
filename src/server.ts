badd66b345594480895c0a4a454a9660aac014e4
jaydenabdulvohid <hakimovabdulvohid85@gmail.com>
2024-02-22 20:32:13 +0900

    BRR: Initial SetUp

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
+// Architectural pattern: MVC = Module view controler, DI = Dependency Injection, MVP =Module view Presenter => Artichectural bu bizni backandimiznin suyagi yani backandimizdagi malumotlar oqimini tartbga soladigan bir vosita
 
+// MVC = Module view controler
+
+// MVP = Module view Presenter
+
+// DI = Dependency Injection -> Bu asosan Modullarni ishlatish tizimida ishlatilinadi yani NesJs framwork mavjut NesJS Butunlay shu Dpendency Injection  ni ustiga qurilgan
+
+// Design pattern: Middleware, Decotar => Butun bir backand tizimimizni tartbga soluvchi sifatida emas vositasi siatida emas malum bir bolaklarni struckturasini yechishda xizmat qiladigan patternlar xisoblanadi
