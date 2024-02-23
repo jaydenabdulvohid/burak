a3f3547464a92a7e5170f3751668463478846af3
jaydenabdulvohid <hakimovabdulvohid85@gmail.com>
2024-02-23 12:59:25 +0900

    feat: integration of invoaremental variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-// Architectural pattern: MVC = Module view controler, DI = Dependency Injection, MVP =Module view Presenter => Artichectural bu bizni backandimiznin suyagi yani backandimizdagi malumotlar oqimini tartbga soladigan bir vosita
-
-// MVC = Module view controler
-
-// MVP = Module view Presenter
-
-// DI = Dependency Injection -> Bu asosan Modullarni ishlatish tizimida ishlatilinadi yani NesJs framwork mavjut NesJS Butunlay shu Dpendency Injection  ni ustiga qurilgan
-
-// Design pattern: Middleware, Decotar => Butun bir backand tizimimizni tartbga soluvchi sifatida emas vositasi siatida emas malum bir bolaklarni struckturasini yechishda xizmat qiladigan patternlar xisoblanadi
+// import moment  from "moment";   // const moment = requiere("moment");->commonJS
+import dotenv from "dotenv";
+dotenv.config();
