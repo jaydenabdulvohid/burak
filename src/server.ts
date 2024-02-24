254f54f4ea1736e4f2a5e49d69670e43ae7f95ba
jaydenabdulvohid <hakimovabdulvohid85@gmail.com>
2024-02-24 15:15:56 +0900

    feat: connect mogoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
-// import moment  from "moment";   // const moment = requiere("moment");->commonJS
 import dotenv from "dotenv";
 dotenv.config();
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERROR on cennection MogoDB", err));
