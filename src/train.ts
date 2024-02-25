4e1e91a2405c651fe4b8ce3b04c0a22d5de6d160
jaydenabdulvohid <hakimovabdulvohid85@gmail.com>
2024-02-25 17:40:58 +0900

    Train: H-task yechimi

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,13 @@
 console.log("BISMILLAH");
+function gerReverse(str: string): string {
+  return str.split("").reverse().join("");
+}
+
+// Test
+console.log(gerReverse("hello")); // "olleh"
+
+// function getReverse(str: string): string {
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(getReverse("salom"));
