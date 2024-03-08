5940052ab5bc099d4b72f51b8d09bfbe81ae43c5
jaydenabdulvohid <hakimovabdulvohid85@gmail.com>
2024-03-08 16:56:57 +0900

    Train: J-task yechimi

--- src/train.ts
+++ src/train.ts
@@ -1,70 +1,82 @@
-// I -task yechimi
-/**
- * Berilgan stringda bo'lgan raqamlarni ajratib olib yangi string sifatida qaytaradi.
- *
- * @param str String, ichida raqamlar bo'lishi mumkin.
- * @returns Ajratilgan raqamlardan iborat string.
- */
-function getDigits(str: string): string {
-  // Stringdan faqat raqamlarni ajratib olamiz
-  const digitsOnly = str.replace(/\D/g, "");
-
-  return digitsOnly;
+function findLongestWord(s: string): string {
+  const words: string[] = s.split(" "); // stringni so'zlarga bo'lib ajratamiz
+  const longestWord: string = words.reduce((prevWord, currentWord) => {
+    return prevWord.length >= currentWord.length ? prevWord : currentWord;
+  }, "");
+  return longestWord;
 }
 
-// Test
-console.log(getDigits("m14i1t")); // "141"
+// Misol
+const result: string = findLongestWord("I come from Uzbekistan");
+console.log(result); // "Uzbekistan" chiqadi
 
-/* Project Standarts:
-  - Login standarts
-  - Naming Starndarts
-         function methodi variable => CAMEL case goHome
-         Classlar => PASCAL case             
-         folderlar, file => KEBAB                                    
-         css classlar => SNAKE    
-    - Error handling                               
+// // I -task yechimi
+// /**
+//  * Berilgan stringda bo'lgan raqamlarni ajratib olib yangi string sifatida qaytaradi.
+//  *
+//  * @param str String, ichida raqamlar bo'lishi mumkin.
+//  * @returns Ajratilgan raqamlardan iborat string.
+//  */
+// function getDigits(str: string): string {
+//   // Stringdan faqat raqamlarni ajratib olamiz
+//   const digitsOnly = str.replace(/\D/g, "");
 
-*/
-
-/**
-  Traditional Api
-  Rest Api
-  GraphQl Api
-  ...
- */
-
-/**
- * Traditional Frontend Development => BSSR -> (ADMIN) Bacakent server site rendring => EJS freamwork
- * Modern Frontent Development      => SPA (USER, aplication) -> Single page Aplication => REACT
-
- */
-
-// console.log("BISMILLAH");
-
-/* H-task yechimi
-// function getPositive(arr: number[]): string {
-//   // Arrayni musbat qiymatlarini olib qaytiramiz
-//   const positiveNumbers = arr.filter((num) => num > 0);
-
-//   // Musbat qiymatlarni stringga aylantiramiz
-//   const positiveString = positiveNumbers.join("");
-
-//   return positiveString;
+//   return digitsOnly;
 // }
 
 // // Test
-// console.log(getPositive([1, -4, 2])); // "12"
+// console.log(getDigits("m14i1t")); // "141"
 
-// function gerReverse(str: string): string {
-//   return str.split("").reverse().join("");
-// }
+// /* Project Standarts:
+//   - Login standarts
+//   - Naming Starndarts
+//          function methodi variable => CAMEL case goHome
+//          Classlar => PASCAL case
+//          folderlar, file => KEBAB
+//          css classlar => SNAKE
+//     - Error handling
 
-// // Test
-// console.log(gerReverse("hello")); // "olleh"
+// */
 
-// function getReverse(str: string): string {
-//   return str.split("").reverse().join("");
-// }
+// /**
+//   Traditional Api
+//   Rest Api
+//   GraphQl Api
+//   ...
+//  */
 
-// console.log(getReverse("salom"));
-*/
+// /**
+//  * Traditional Frontend Development => BSSR -> (ADMIN) Bacakent server site rendring => EJS freamwork
+//  * Modern Frontent Development      => SPA (USER, aplication) -> Single page Aplication => REACT
+
+//  */
+
+// // console.log("BISMILLAH");
+
+// /* H-task yechimi
+// // function getPositive(arr: number[]): string {
+// //   // Arrayni musbat qiymatlarini olib qaytiramiz
+// //   const positiveNumbers = arr.filter((num) => num > 0);
+
+// //   // Musbat qiymatlarni stringga aylantiramiz
+// //   const positiveString = positiveNumbers.join("");
+
+// //   return positiveString;
+// // }
+
+// // // Test
+// // console.log(getPositive([1, -4, 2])); // "12"
+
+// // function gerReverse(str: string): string {
+// //   return str.split("").reverse().join("");
+// // }
+
+// // // Test
+// // console.log(gerReverse("hello")); // "olleh"
+
+// // function getReverse(str: string): string {
+// //   return str.split("").reverse().join("");
+// // }
+
+// // console.log(getReverse("salom"));
+// */
